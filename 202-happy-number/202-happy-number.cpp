8473e62a639686621afc6a797527cd1d5499aa52
Shivam Batra <63354292+shivam18213@users.noreply.github.com>
2022-01-21 22:47:50 +0530

    Time: 3 ms (48.89%), Space: 6.3 MB (25.61%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int sum_digits(int n){
+        int sum1= 0;
+        int d=0;
+        while(n > 0){
+            d= n % 10;
+            sum1 = sum1+ (d * d);
+            n = n / 10;
+        }
+        return sum1;
+    }
+    bool isHappy(int n) {
+        int sum=0;
+        set<int> s;
+        s.insert(n);
+        while(n != 1){
+            sum = sum_digits(n);
+            if(s.find(sum) != s.end()){
+                return false;
+            }
+            n=sum;
+            s.insert(sum);
+        }
+        return n==1;
+    }
+};

dde1c7e521c3a55ab7e82c4ba843285a734a2d1e
Shivam Batra <63354292+shivam18213@users.noreply.github.com>
2022-07-13 08:11:27 +0530

    Time: 59 ms (18.39%), Space: 13.5 MB (12.97%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        if(s.size() == 1){
+    int lengthOfLongestSubstring(string st) {
+        if(st.size() == 0){
+            return 0;
+        }
+        if(st.size() == 1){
             return 1;
         }
-        int i=0;
-        int j=0;
+        set<char> s;
+        int i = 0;
+        int j = 0;
         int maxLen=0;
-        vector<int> chars(256,0);
-        while(j<s.size()){
-            chars[int(s[j])]++;
-            while(chars[int(s[j])] > 1){
-                chars[int(s[i])]--;
+        while(j < st.size()){
+            if(s.empty()){
+                s.insert(st[j]);
+                j++;
+            }
+            else if(!s.empty() && s.find(st[j]) != s.end()){
+                maxLen = max(maxLen, j-i);
+                s.erase(st[i]);
                 i++;
             }
-            maxLen = max(maxLen, j-i+1);
-            j++;
+            else if(!s.empty() && s.find(st[j]) == s.end()){
+                s.insert(st[j]);
+                j++;
+                
+            }
+            //cout<<maxLen<<"asdas\n";
+            
         }
-        return maxLen;
+        return max(maxLen,j-i);
     }
 };
